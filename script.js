b286d0ed5ed3167411fa9de5b0d289bae7d3742b
Dustin Condello <dustincondello@googlemail.com>
2025-04-10 18:39:45 +0200

    add json questions

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Ronaldo",
+        "answer_2": "Tim Berners-Lee", 
+        "answer_3": "Messi",
+        "answer_4": "Bill Gates",
+        "right_answer": 2,
+    },
+    {
+        "question": "Was ist das Hauptziel von HTML?",
+        "answer_1": "Datenbankabfragen",
+        "answer_2": "Webseiten strukturieren", 
+        "answer_3": "Server konfigurieren",
+        "answer_4": "Betriebssysteme programmieren",
+        "right_answer": 2,
+    },
+    {
+        "question": "Was bedeutet CSS?",
+        "answer_1": "Cascading Style Sheets",
+        "answer_2": "Computer Style Sheets", 
+        "answer_3": "Creative Style Sheets",
+        "answer_4": "Colorful Style Sheets",
+        "right_answer": 1,
+    },
+    {
+        "question": "Was ist das Hauptziel von CSS?",
+        "answer_1": "Webseiten strukturieren",
+        "answer_2": "Webseiten gestalten", 
+        "answer_3": "Webseiten programmieren",
+        "answer_4": "Webseiten testen",
+        "right_answer": 2,
+    },
+    {
+        "question": "Was ist JavaScript?",
+        "answer_1": "Eine Programmiersprache",
+        "answer_2": "Ein Datenbankmanagementsystem", 
+        "answer_3": "Ein Webserver",
+        "answer_4": "Ein Betriebssystem",
+        "right_answer": 1,
+    },
+]
