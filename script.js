54e2b562becca71ae8ad09c5b2ddc7629c69447e
Dustin Condello <dustincondello@googlemail.com>
2025-04-10 20:47:39 +0200

    button with right/false button onclick function

--- script.js
+++ script.js
@@ -40,3 +40,39 @@ let questions = [
         "right_answer": 1,
     },
 ]
+
+let currentQuestion = 0;
+
+
+function init(){
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showQuestion();
+}
+
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection){
+    let question = questions[currentQuestion];
+
+    console.log('slected number is', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selected question number is', selectedQuestionNumber);
+    console.log('right answer is', question['right_answer']);
+    
+    if(selectedQuestionNumber == question['right_answer']){
+        console.log('Richtige Antwort');
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        console.log('Falsche Antwort');
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+    }
+}
